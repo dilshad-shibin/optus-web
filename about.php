7c4af8e6dd6dad10bcb0b4cfd7806767ac928448
Dilshad Shibin <49745082+dilshad-shibin@users.noreply.github.com>
2021-05-27 13:02:29 +0530

    about us modified

--- about.php
+++ about.php
@@ -113,7 +113,7 @@
 <div class="mis_sec_left pr-lg-3 wow fadeInUp">
 <h5></h5>
 <h3 class="main_title ">Why Choose Optus Healthcare? <div class="t_line"><span></span></div></h3>    
-<ul>
+<!-- <ul>
     <li><i class="fa fa-check-square-o"></i> Fast and Speedy procees</li>
     <li><i class="fa fa-check-square-o"></i> Regular weekly interviews with best UK employers with NHS and provate</li>
     <li><i class="fa fa-check-square-o"></i> Friendly and knowledgeable staff</li>
@@ -121,7 +121,10 @@
     <li><i class="fa fa-check-square-o"></i> Scholarly CBT Training</li>
     <li><i class="fa fa-check-square-o"></i> Fast deployment</li>
     <li><i class="fa fa-check-square-o"></i> Mock Interview sessions to brushne up your skills </li>
-</ul>
+</ul> -->
+<p>The demand for healthcare staff globally is increasing; Optus has responded with strong specialist teams based in the UK and India. We have also developed recruitment partnerships and presence across many other countries around the world.</p>
+<p>Our international team offers bespoke, end-to-end solutions and provides professional support to clients and candidates throughout the recruitment process. </p>
+<p>We specialize in the permanent placement of doctors, psychiatrists, nurses, and allied health professionals from all over the world. Since our inception, we’ve been placing qualified candidates in the UK, New Zealand, Australia, and the Middle East and have established a reputation for our exceptional service.</p>
 </div>
 </div>
 <div class="col-lg-6">
@@ -159,6 +162,49 @@
                     <p>The current COVID -19 pandemic has showed the world that how much we need to invest in healthcare staffing and there is no better way to organise your staffing to ensure that you are equipped to handle such cirsis in the  future. Finding and hiring skilled talent is more significant than ever before. </p>
                     <p>OPTUS HEALTHCARE LIMITED has the tools to help you get the quality healthcare workforce that you have always desired. Speak to us today to see how we can help in helping you attain the required levels of staffing.</p>
                 </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="mission_sec" style="padding-top: 130px">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 align_items_center">
+                <div class="mis_sec_left pr-lg-3 wow fadeInUp" style="margin-bottom:50px">
+                    <h5></h5>
+                    <h3 class="main_title text-center">Why work abroad with Optus ?<div class="t_line"><span></span></div></h3>    
+                    <p class="text-center">Whatever your destination, we will find you the best jobs worldwide and support you through the relocation process. </p>    
+                </div>
+            </div>
+        </div>
+        <div class="row align-items-center">
+            <div class="col-lg-6 mb-3">
+                    <h5 style="margin-bottom:20px"><strong>Benefits and rewards:</strong></h5>
+                    <ul>
+                        <li><i class="fa fa-check-square-o"></i> Competitive rates of pay </li>
+                        <li><i class="fa fa-check-square-o"></i> Access to a wide choice of temporary and permanent healthcare jobs worldwide</li>
+                        <li><i class="fa fa-check-square-o"></i> First-class support and advice to help you settle into your new life abroad</li>
+                        <li><i class="fa fa-check-square-o"></i> Streamlined registration and compliance process</li>
+                        <li><i class="fa fa-check-square-o"></i> Guidance with international professional body registration</li>
+                        <li><i class="fa fa-check-square-o"></i> Support and funding towards your registration process</li>
+                        <li><i class="fa fa-check-square-o"></i> Flexible contract options to suit your lifestyle </li>
+                        <li><i class="fa fa-check-square-o"></i> Employment opportunities with visa sponsorship</li>
+                        <li><i class="fa fa-check-square-o"></i> Placements with world-leading hospitals around the world</li>
+                        <li><i class="fa fa-check-square-o"></i> Recommend a friend scheme</li>
+                        <li><i class="fa fa-check-square-o"></i> Assistance arranging travel and finding accommodation (if applicable) </li>
+                        <li><i class="fa fa-check-square-o"></i> Dependent on contracts available at the time of joining</li>
+                    </ul>
+            </div>
+            <div class="col-lg-6">
+                    <div class="mis_sec_right pl-lg-3">
+                        <div class="comn_img_block wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.2s">
+                            <img src="images/about/nurse.jpg">
+                        </div> 
+                        <div class="com_circle_box1 float_right"></div>
+                        <div class="com_circle_box2 float_up"></div>
+                    </div>
             </div>
         </div>
     </div>
